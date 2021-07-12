74c5caa04bef0fdf55941f010af9f953bd2592d3
chunyuan <1147688450@qq.com>
2021-07-12 15:18:06 +0800

    first commit

---
+++ src/RecordLog.php
@@ -0,0 +1,23 @@
+<?php
+
+
+class RecordLog
+{
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * 记录日志
+     * @param $content
+     */
+    public static function log($content)
+    {
+        if (is_array($content)) {
+            file_put_contents('log.txt', date('Y-m-d H:i:s') . ' ' . print_r($content, true) . "\n", FILE_APPEND);
+        }else{
+            file_put_contents('log.txt', date('Y-m-d H:i:s') . ' ' . $content . "\n", FILE_APPEND);
+        }
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'bajieeeee/record-log',
+  ),
+  'versions' => 
+  array (
+    'bajieeeee/record-log' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
