7f9b1d869535344b2445eac85c10c8213c989105
chunyuan <1147688450@qq.com>
2021-07-13 14:58:15 +0800

    修改命名空间-6

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,17 +4,17 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInitc2d8ca099536a382c146aab7a671a76f
+class ComposerStaticInitf7f5a0f0c604c854b3cbffe7e039929e
 {
     public static $prefixLengthsPsr4 = array (
         'B' => 
         array (
-            'Bajieeeee\\record\\' => 17,
+            'Bajieeeee\\' => 10,
         ),
     );
 
     public static $prefixDirsPsr4 = array (
-        'Bajieeeee\\record\\' => 
+        'Bajieeeee\\' => 
         array (
             0 => __DIR__ . '/../..' . '/src',
         ),
@@ -27,9 +27,9 @@ class ComposerStaticInitc2d8ca099536a382c146aab7a671a76f
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInitc2d8ca099536a382c146aab7a671a76f::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInitc2d8ca099536a382c146aab7a671a76f::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInitc2d8ca099536a382c146aab7a671a76f::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitf7f5a0f0c604c854b3cbffe7e039929e::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitf7f5a0f0c604c854b3cbffe7e039929e::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInitf7f5a0f0c604c854b3cbffe7e039929e::$classMap;
 
         }, null, ClassLoader::class);
     }
